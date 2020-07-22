b8f3c018bfd021fdde080bc2e390523bd8a4d7f4
Armando1424 <armandowells24@gmail.com>
2020-07-22 16:09:45 -0500

    Little things.

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
         }
     }
 }
