fcfb73b0809e17d76797e72c772c57483bb65f5f
Armando1424 <armandowells24@gmail.com>
2020-07-22 18:53:36 -0500

    The client emulator is almost ready.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,16 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Client
+﻿namespace Client
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Program
     {
+        public static TcpConnection connection = new TcpConnection();
+        public static string IPADDRESS = "127.0.0.1";
+        public const int port = 1997;
+
         public static void Main(string[] args)
         {
+            Load();
+        }
 
+        private static void Load()
+        {
+            connection.OnDataRecieved += MessageReceived;
+
+            if (!connection.ToConnect(IPADDRESS,port))
+            {
+                Console.WriteLine("Error conectando con el servidor");
+                return;
+            }
+
+            if (connection.TcpClient.Connected)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    connection.SendMessage("hola");
+                }
+            }
+        }
+
+        private static void MessageReceived(string data)
+        {
+            //here call or put the logic for to clasificate the message and display it.
+            if (data == "StatusReport")
+            {
+                
+            }
+        }
+
+        private static void displayData()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Client/TcpConnection.cs
@@ -0,0 +1,122 @@
+﻿namespace Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class TcpConnection
+    {
+        public TcpClient TcpClient { get; private set; }
+        public NetworkStream Stream { get; private set; }
+        public Thread ReadThread { get; private set; }
+        public StreamWriter Writer { get; private set; }
+
+        public delegate void DataCarrier(string data);
+        public event DataCarrier OnDataRecieved;
+
+        public delegate void DisconnectNotify();
+        public event DisconnectNotify OnDisconnect;
+
+        public delegate void ErrorCarrier(Exception e);
+        public event ErrorCarrier OnError;
+
+        public bool ToConnect(string ipAddress, int port)
+        {
+            try
+            {
+                TcpClient = new TcpClient();
+                TcpClient.Connect(IPAddress.Parse(ipAddress), port);
+                Stream = TcpClient.GetStream();
+                Writer = new StreamWriter(Stream);
+                ReadThread = new Thread(ToListen);
+                ReadThread.Start();
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (OnError != null)
+                {
+                    OnError(e);
+                }
+                return false;
+            }
+        }
+
+        private void ToListen()
+        {
+            var reader = new StreamReader(Stream);
+            var charBuffer = new List<int>();
+
+            do
+            {
+                try
+                {
+                    if (reader.EndOfStream)
+                    {
+                        break;
+                    }
+                    int charCode = reader.Read();
+                    if (charCode == -1)
+                    {
+                        break;
+                    }
+                    if (charCode != 0)
+                    {
+                        charBuffer.Add(charCode);
+                        continue;
+                    }
+                    if (OnDataRecieved != null)
+                    {
+                        var chars = new char[charBuffer.Count];
+                        for (int i = 0; i < charBuffer.Count; i++)
+                        {
+                            chars[i] = Convert.ToChar(charBuffer[i]);
+                        }
+                        var message = new string(chars);
+                        OnDataRecieved(message);
+                    }
+                    charBuffer.Clear();
+                }
+                catch (Exception e)
+                {
+                    if (OnError != null)
+                    {
+                        OnError(e);
+                    }
+                    break;
+                }
+            } while (true);
+            if (OnDisconnect != null)
+            {
+                OnDisconnect();
+            }
+        }
+
+        private void WriteMessage(string message)
+        {
+            try
+            {
+                Writer.Write(message + "\0");
+                Writer.Flush();
+            }
+            catch (Exception e)
+            {
+                if (OnError != null)
+                {
+                    OnError(e);
+                }
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            WriteMessage(message);
+        }
+    }
+}
